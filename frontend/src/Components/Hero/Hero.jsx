45e75ac69b3a73b618e1add9120f7d3292238a6c
adarsh <opensourcetech47@gmail.com>
2026-06-09 18:07:58 +0530

    Make Responsive

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,50 +2,45 @@ import { Link } from "react-router-dom";
 
 export const Hero = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col lg:flex-row items-center overflow-hidden">
-      <div className="flex-1 flex flex-col justify-center gap-6 px-6 md:px-20 lg:pl-32 py-16 lg:py-0">
+    <div className="lg:min-h-screen bg-gray-100 flex lg:flex-row items-center overflow-hidden">
+      <div className="flex-1 flex flex-col justify-center gap-6 px-6 md:px-20 lg:pl-32 lg:py-0">
         <div className="space-y-2">
-          <h2 className="text-black text-lg md:text-xl font-bold tracking-widest text-center lg:text-left uppercase">
+          <h2 className="text-black text-3xl lg:text-xl font-bold tracking-widest text-center lg:text-left uppercase">
             New Arrivals Only
           </h2>
-          <div className="h-1 w-20 bg-black mx-auto lg:mx-0 rounded-full"></div>
+          <hr className="bg-black lg:w-30 h-2 rounded-full" />
         </div>
 
-        <div className="text-center lg:text-left">
-          <div className="flex items-center justify-center lg:justify-start gap-4">
-            <p className="text-[#171717] text-6xl md:text-[90px] font-extrabold leading-none tracking-tight">
-              New
-            </p>
-          </div>
-          <p className="text-[#171717] text-6xl md:text-[90px] font-extrabold leading-none capitalize">
-            collections for everyone
+        <div className="text-left">
+          <p className="text-black lg:text-[90px] text-[60px] font-extrabold leading-none tracking-tight ">
+            New Collections For Everyone
           </p>
         </div>
 
-        <p className="text-black text-lg max-w-md text-center lg:text-left font-medium capitalize">
+        <p className="text-black lg:text-lg text-2xl max-w-md text-left font-medium capitalize">
           Discover the latest trends and redefine your style with our exclusive
           summer pieces.
         </p>
         <Link
           to="/category/mens"
-          className="group relative flex justify-center items-center gap-4 w-64 md:w-72 h-16 rounded-full mt-4 bg-black text-white text-lg font-semibold cursor-pointer self-center lg:self-start transition-all duration-300"
+          className="group relative flex justify-center items-center gap-4 lg:h-16 h-18 rounded-full mt-4 bg-black text-white lg:text-lg text-2xl px-5 py-2 lg:px-10 font-semibold cursor-pointer lg:self-start transition-all duration-300"
         >
           <span>Latest Collection</span>
           <img
             src="https://res.cloudinary.com/dpqggtyjw/image/upload/v1771488923/arrow_okjubu.png"
             alt="arrow icon"
-            className="w-5 group-hover:translate-x-2 transition-transform duration-300"
+            className="lg:w-5 w-8 group-hover:translate-x-2 transition-transform duration-300"
           />
         </Link>
       </div>
 
-      <div className="flex-1 relative flex items-center justify-center p-6 lg:p-0">
-        <div className="absolute w-75 h-75 md:w-125 md:h-125 bg-[#ff414111] rounded-full blur-3xl -z-10"></div>
+      <div className="flex-1 relative flex items-center justify-center">
+        <div className="absolute w-95 h-95 md:w-125 md:h-125 bg-[#ff414111] rounded-full blur-3xl -z-10"></div>
 
         <img
           src="https://res.cloudinary.com/dpqggtyjw/image/upload/v1774426785/Blur_rbdte1.png"
           alt="hero"
-          className="w-full max-w-112.5 md:max-w-150 drop-shadow-2xl hover:scale-[1.02] transition-transform duration-500 ease-in-out object-contain h-182"
+          className="w-full max-w-200 lg:max-w-150 drop-shadow-2xl hover:scale-[1.02] transition-transform duration-500 ease-in-out object-contain lg:h-182"
         />
       </div>
     </div>
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -22,20 +22,20 @@ function Navbar() {
     setIsLoggedIn(!!token);
   }, []);
   return (
-    <nav className="flex items-center justify-between px-4 py-3 lg:px-20 lg:py-5 bg-white shadow-sm sticky top-0 z-50">
+    <nav className="flex items-center justify-between px-10 py-8 lg:px-20 lg:py-5 bg-white shadow-sm sticky top-0 z-50 h-40 lg:h-20">
       <div className="flex items-center gap-2 lg:gap-4">
         <Link to="/" onClick={() => setMenu("shop")}>
           <img
             src="https://res.cloudinary.com/dpqggtyjw/image/upload/v1771500056/logo_big_ayetdt.png"
             alt="logo"
-            className="w-8 lg:w-12"
+            className="lg:w-12 w-20"
           />
         </Link>
-        <p className="text-xl lg:text-3xl font-bold tracking-wider">SHOPPER</p>
+        <p className="lg:text-3xl text-4xl font-bold tracking-wider">SHOPPER</p>
       </div>
 
       <button
-        className="lg:hidden p-2 text-2xl focus:outline-none"
+        className="lg:hidden p-2 lg:text-3xl text-4xl focus:outline-none"
         onClick={toggleMobileMenu}
       >
         {isMobileMenuOpen ? "✕" : "☰"}
@@ -54,7 +54,10 @@ function Navbar() {
             setIsMobileMenuOpen(false);
           }}
         >
-          <Link to="/" className="hover:text-black transition-colors">
+          <Link
+            to="/"
+            className="hover:text-black transition-colors lg:text-[18px] text-2xl"
+          >
             Shop
           </Link>
           {menu === "shop" && (
@@ -70,7 +73,7 @@ function Navbar() {
         >
           <Link
             to="/category/mens"
-            className="hover:text-black transition-colors"
+            className="hover:text-black transition-colors lg:text-[18px] text-2xl"
           >
             Men
           </Link>
@@ -87,7 +90,7 @@ function Navbar() {
         >
           <Link
             to="/category/womens"
-            className="hover:text-black transition-colors"
+            className="hover:text-black transition-colors lg:text-[18px] text-2xl"
           >
             Women
           </Link>
@@ -104,7 +107,7 @@ function Navbar() {
         >
           <Link
             to="/category/kids"
-            className="hover:text-black transition-colors"
+            className="hover:text-black transition-colors lg:text-[18px] text-2xl"
           >
             Kids
           </Link>
@@ -119,20 +122,20 @@ function Navbar() {
           <div className="relative">
             <div
               onClick={() => setShowDropdown(!showDropdown)}
-              className="w-8 h-8 flex items-center justify-center bg-black text-white rounded-full cursor-pointer"
+              className="w-10 h-10 flex items-center justify-center bg-black text-white rounded-full cursor-pointer"
             >
               <CircleUser />
             </div>
             {showDropdown && (
               <>
-                <div className="absolute right-0 mt-2 w-40 bg-white shadow-lg rounded-lg border overflow-hidden ">
-                  <div className="p-2 text-white">
-                    <button className="w-full px-4 py-2 cursor-pointer bg-white text-black hover:bg-gray-200 rounded">
+                <div className="absolute right-0 mt-2 lg:w-40 w-50 bg-white shadow-lg rounded-lg border overflow-hidden ">
+                  <div className="p-2 text-white ">
+                    <button className=" lg:text-[16px] text-2xl w-full px-4 py-2 cursor-pointer bg-white text-black hover:bg-gray-200 rounded">
                       My Orders
                     </button>
                     <button
                       onClick={logout}
-                      className="w-full px-4 py-2 hover:bg-red-600 hover:text-white text-black text-center rounded cursor-pointer "
+                      className="w-full px-4 py-2 hover:bg-red-600 hover:text-white text-black text-center rounded cursor-pointer text-2xl lg:text-[16px] "
                     >
                       Logout
                     </button>
@@ -144,13 +147,13 @@ function Navbar() {
         ) : (
           <>
             <Link to="/login">
-              <button className="w-24 lg:w-30 h-10 lg:h-12 border rounded-full">
+              <button className="px-8 py-2 border rounded-full lg:text-[18px] text-2xl">
                 Login
               </button>
             </Link>
 
             <Link to="/signup">
-              <button className="w-24 lg:w-30 h-10 lg:h-12 border rounded-full">
+              <button className="px-8 py-2 border rounded-full lg:text-[18px] text-2xl">
                 Signup
               </button>
             </Link>
@@ -162,10 +165,10 @@ function Navbar() {
             <img
               src="https://res.cloudinary.com/dpqggtyjw/image/upload/v1772640252/cart_icon_exaar9.png"
               alt="cart"
-              className="w-6"
+              className="lg:w-7 w-10"
             />
           </Link>
-          <div className="absolute -top-2 -right-2 w-4 flex justify-center items-center rounded-full lg:text-[10px] bg-black text-white">
+          <div className="absolute -top-3 -right-3 lg:p-2 sm:p-3 w-5 h-5 flex justify-center items-center rounded-full lg:text-[12px] bg-black text-white">
             {getTotalCartItems()}
           </div>
         </div>
